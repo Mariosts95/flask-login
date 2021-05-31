6ba2dc3d3eef813c6308cce538ec36625350f0e7
Marios Tsioutsis <72659932+Mariosts95@users.noreply.github.com>
2021-05-31 23:38:34 +0300

    Add logout functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from os import environ
-from flask import Flask, render_template, url_for, redirect, request, flash, session
+from flask import Flask, render_template, url_for, redirect, request, flash, session, abort
 
 app = Flask(__name__)
 # get the sercet key for the session from .flaskenv
@@ -22,23 +22,26 @@ def loginAuth():
     password = request.form.get('password')
 
     if username == 'admin' and password == 'admin':
-        session['username'] = username
-        flash('Login successful')
+        session['logged_in'] = username
         return redirect(url_for('profile', username=username))
     else:
-        flash('login error')
+        flash('Username or password invalid')
         return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
-    return render_template('login.html')
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
 
 
 @app.route('/profile')
 @app.route('/profile/<username>')
 def profile(username):
-    return render_template('profile.html', username=username)
+    if not session['logged_in'] == username:
+        abort(401)
+    else:
+        return render_template('profile.html', username=username)
 
 
 # Handles the error when a path doesn't exist
@@ -46,6 +49,12 @@ def profile(username):
 def page_not_found(e):
     app.logger.debug(e)
     return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(401)
+def page_forbidden(e):
+    app.logger.debug(e)
+    return redirect(url_for('login')), 401
 
 
 if __name__ == '__main__':
